03fce2450133778cf9fb1dc6dd8e2dd6ad7b4a92
Rahul <luharihsin@gmail.com>
2020-05-13 22:05:49 +0530

    added debug msgs for compute server

--- compute_profile.sh
+++ compute_profile.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+function statusUpdate() {
+ echo -e "${1}"" ""${2}..."
+}
+
+
 SElinux="/etc/sysconfig/selinux"
 HOST_NAME="/etc/hostname"
 HOST_ONLY_CON="/etc/sysconfig/network-scripts/ifcfg-ens33"
@@ -7,17 +12,23 @@ DNS="/etc/hosts"
 AUTH_KEYS="/root/.ssh/authorized_keys"
 SSH_CONFIG="/root/.ssh/config"
 
+
+statusUpdate 'changing' 'hostname'
 if [ -f ${HOST_NAME} ]; then
         echo "cp01">${HOST_NAME}
 fi
 
+statusUpdate 'disabling' 'NetworkManager'
 systemctl stop NetworkManager.service
 systemctl disable NetworkManager.service
 
+
+statusUpdate 'disabling' 'SElinux'
 if [ -f ${SElinux} ]; then
         echo "SELINUX=disabled">${SElinux}
 fi
 
+statusUpdate 'configuring' 'Host-Only adapter'
 if [ -f ${HOST_ONLY_CON} ]; then
 cat <<EOF > ${HOST_ONLY_CON}
 TYPE=Ethernet
@@ -32,6 +43,8 @@ GATEWAY=192.168.225.100
 EOF
 fi
 
+
+statusUpdate 'configuring' 'DNS'
 if [ -f ${DNS} ]; then
 cat <<EOF >> $DNS
 192.168.225.100 sp header main
@@ -40,11 +53,17 @@ cat <<EOF >> $DNS
 EOF
 fi
 
+
+statusUpdate 'restarting' 'network'
 systemctl restart network
 
+
+statusUpdate 'disabling' 'firewall'
 systemctl stop firewalld.service
 systemctl disable firewalld.service
 
+
+statusUpdate 'checking' 'ssh'
 if [ -d $HOME/.ssh ];then
 rm -rf $HOME/.ssh
 fi
@@ -53,9 +72,11 @@ if [ -z $SSH_AGENT_PID ]; then
 eval `ssh-agent`
 fi
 
-echo -e '\n\n\n'|ssh-keygen
 
-ssh-add
+statusUpdate 'creating' 'ssh-keys'
+echo -e '\n\n\n'|ssh-keygen 1> /dev/null 2>&1
+
+ssh-add 1>/dev/null 2>&1
 
 cat <<EOF > ${SSH_CONFIG}
 Host *
